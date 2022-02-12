bf12619ef9d6f7bf592e168043163df077aa7fdc
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-11 23:49:18 -0800

    finished launch countdown

--- loopPractice/LaunchCountdown/javaScript.js
+++ loopPractice/LaunchCountdown/javaScript.js
@@ -1,8 +1,23 @@
 let output = document.querySelector('.output');
 output.innerHTML = '';
 
-// let i = 10;
+let i = 10;
+while (i >= 0) {
+
+    const para = document.createElement('p');
+    if (i === 10) {
+        para.textContent = `Countdown ${i}...`;
+    } 
+    else if(i < 10 && i > 0) {
+        para.textContent = `${i}...`;
+    }
+    else{
+        para.textContent = "Blast Off!!!";
+    }
+    i--;
+    output.appendChild(para);
+}
 
 // const para = document.createElement('p');
 // para.textContent = ;
-// output.appendChild(para);
+//output.appendChild(para);
